0d94a646bc87fc626b3cf3d0d037b5a153c5dd99
pocemonius <ipocemonius@gmail.com>
2019-09-06 15:24:48 +0300

    Create info_from_infoblock.php

---
+++ info_from_infoblock.php
@@ -0,0 +1,28 @@
+$db_Iblock = CIBlock::GetList(  array(),   array("CODE"=> "PRODUCTS_DOCUMENTS"));
+if($ar_Iblock =$db_Iblock -> Fetch())
+{
+
+    $db_section = CIBlockSection::GetList( array ("SORT"=>"ASC"),Array("IBLOCK_ID" => $ar_Iblock["ID"], "ACTIVE"=> "Y"));
+    while( $ar_section  =  $db_section -> Fetch())
+        {
+
+            ?>
+                <tr>
+                    <td class="title" colspan="2"><?=$ar_section["NAME"]?></td>
+                </tr>    
+            <?
+
+
+            $db_els = CIBlockElement::GetList(Array(),Array("IBLOCK_ID" => $ar_Iblock["ID"], "ACTIVE"=> "Y", "IBLOCK_SECTION_ID"=> $ar_section["ID"]));
+            while($ar_els = $db_els-> Fetch())
+                {
+                    ?>
+                        <tr>                            
+                            <td><?=$ar_els["PREVIEW_TEXT"]?></td>
+                            <td><?=$ar_els["NAME"]?></td>
+                        </tr>
+                    <?
+                }
+        }
+
+}
